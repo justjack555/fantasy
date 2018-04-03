cace841bc4d4c5f9ca05b9fd5163ea8dcc1ecd4c
Forrest Hofmann <Forrest@dyn-160-39-133-212.dyn.columbia.edu>
2018-04-03 17:34:05 -0400

    Created route definitions and preliminary login functionality

--- server.py
+++ server.py
@@ -1,210 +1,193 @@
 #!/usr/bin/env python2.7
-
-"""
-Columbia's COMS W4111.001 Introduction to Databases
-Example Webserver
-
-To run locally:
-
-    python server.py
-
-Go to http://localhost:8111 in your browser.
-
-A debugger such as "pdb" may be helpful for debugging.
-Read about it online.
-"""
+'''
+python server.py
+http://localhost:8111
+'''
 
 import os
 from sqlalchemy import *
 from sqlalchemy.pool import NullPool
-from flask import Flask, request, render_template, g, redirect, Response
+from flask import Flask, request, render_template, g, redirect, Response, session
 
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
-app = Flask(__name__, template_folder=tmpl_dir)
-
-
-#
-# The following is a dummy URI that does not connect to a valid database. You will need to modify it to connect to your Part 2 database in order to use the data.
-#
-# XXX: The URI should be in the format of: 
-#
-#     postgresql://USER:PASSWORD@35.227.79.146/proj1part2
-#
-# For example, if you had username gravano and password foobar, then the following line would be:
-#
-#     DATABASEURI = "postgresql://gravano:foobar@35.227.79.146/proj1part2"
-#
-DATABASEURI = "postgresql://user:password@35.227.79.146/proj1part2"
-
-
-#
-# This line creates a database engine that knows how to connect to the URI above.
-#
+client_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'client')
+app = Flask(__name__, template_folder=client_folder)
+app.secret_key = 'ABZr98j/3yX R~XHH!jmx]LWX/,7RT'
+DATABASEURI = 'postgresql://fhh2112:JustJackForrest555@35.227.79.146/proj1part2'
 engine = create_engine(DATABASEURI)
-
-#
-# Example of running queries in your database
-# Note that this will probably not work if you already have a table named 'test' in your database, containing meaningful data. This is only an example showing you how to run queries in your database using SQLAlchemy.
-#
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
-
 
 @app.before_request
 def before_request():
-  """
-  This function is run at the beginning of every web request 
-  (every time you enter an address in the web browser).
-  We use it to setup a database connection that can be used throughout the request.
-
-  The variable g is globally accessible.
-  """
-  try:
-    g.conn = engine.connect()
-  except:
-    print "uh oh, problem connecting to database"
-    import traceback; traceback.print_exc()
-    g.conn = None
+	'''
+	Setup a database connection that can be used throughout the request.
+	'''
+	try:
+		g.conn = engine.connect()
+	except:
+		print('Error connecting to database.')
+		import traceback
+		traceback.print_exc()
+		g.conn = None
 
 @app.teardown_request
 def teardown_request(exception):
-  """
-  At the end of the web request, this makes sure to close the database connection.
-  If you don't, the database could run out of memory!
-  """
-  try:
-    g.conn.close()
-  except Exception as e:
-    pass
+	'''
+	Close the database connection.
+	'''
+	try:
+		g.conn.close()
+	except Exception as e:
+		pass
 
-
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: http://flask.pocoo.org/docs/0.10/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-  """
-  request is a special object that Flask provides to access web request information:
+	'''
+	GET: Render the index page with static data only
+	'''
+	return render_template('index.html')
 
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
+@app.route('/users/signup/', methods=['GET', 'POST'])
+def signup():
+	'''
+	GET: Render the signup page with static data only
+	POST: Process user signup and user login and redirect to team page
+	      Form should contain login key and password key
+	'''
+	if request.method == 'GET':
+		return render_template('signup.html')
+	else:
+		if 'login' in session:
+			session.pop('login', None)
+		cursor = g.conn.execute('SELECT MAX(user_id) FROM users')
+		max_user_id = cursor.fetchone()[0]
+		try:
+			g.conn.execute('INSERT INTO users VALUES (%d, %s, %s, 0)', max_user_id + 1, request.form['login'], request.form['password'])
+		except:
+			return render_template('signup.html')
+		session['login'] = request.form['login']
+		return redirect('/teams/')
 
-  See its API: http://flask.pocoo.org/docs/0.10/api/#incoming-request-data
-  """
-
-  # DEBUG: this is debugging code to see what request looks like
-  print request.args
-
-
-  #
-  # example of a database query
-  #
-  cursor = g.conn.execute("SELECT name FROM test")
-  names = []
-  for result in cursor:
-    names.append(result['name'])  # can also be accessed using result[0]
-  cursor.close()
-
-  #
-  # Flask uses Jinja templates, which is an extension to HTML where you can
-  # pass data to a template and dynamically generate HTML based on the data
-  # (you can think of it as simple PHP)
-  # documentation: https://realpython.com/blog/python/primer-on-jinja-templating/
-  #
-  # You can see an example template in templates/index.html
-  #
-  # context are the variables that are passed to the template.
-  # for example, "data" key in the context variable defined below will be 
-  # accessible as a variable in index.html:
-  #
-  #     # will print: [u'grace hopper', u'alan turing', u'ada lovelace']
-  #     <div>{{data}}</div>
-  #     
-  #     # creates a <div> tag for each element in data
-  #     # will print: 
-  #     #
-  #     #   <div>grace hopper</div>
-  #     #   <div>alan turing</div>
-  #     #   <div>ada lovelace</div>
-  #     #
-  #     {% for n in data %}
-  #     <div>{{n}}</div>
-  #     {% endfor %}
-  #
-  context = dict(data = names)
-
-
-  #
-  # render_template looks in the templates/ folder for files.
-  # for example, the below file reads template/index.html
-  #
-  return render_template("index.html", **context)
-
-#
-# This is an example of a different path.  You can see it at:
-# 
-#     localhost:8111/another
-#
-# Notice that the function name is another() rather than index()
-# The functions for each app.route need to have different names
-#
-@app.route('/another')
-def another():
-  return render_template("another.html")
-
-
-# Example of adding new data to the database
-@app.route('/add', methods=['POST'])
-def add():
-  name = request.form['name']
-  g.conn.execute('INSERT INTO test(name) VALUES (%s)', name)
-  return redirect('/')
-
-
-@app.route('/login')
+@app.route('/users/login/', methods=['GET', 'POST'])
 def login():
-    abort(401)
-    this_is_never_executed()
+	'''
+	GET: Render the login page with static data only
+	POST: Process user login and redirect to team page
+	      Form should contain login key and password key
+	'''
+	if request.method == 'GET':
+		if 'login' in session:
+			return redirect('/teams/')
+		return render_template('login.html')
+	else:
+		cursor = g.conn.execute('SELECT login, password FROM users')
+		for row in cursor:
+			if row['login'] == request.form['login'] and row['password'] == request.form['password']:
+				session['login'] = request.form['login']
+				return redirect('/teams/')
+		return render_template('login.html')
 
+@app.route('/users/logout/', methods=['GET'])
+def logout():
+	'''
+	GET: Process user logout and redirect to index page
+	'''
+	session.pop('login', None)
+	return redirect('/')
 
-if __name__ == "__main__":
-  import click
+@app.route('/teams/', methods=['GET'])
+def teams():
+	'''
+	GET: Render the team page with team's players and stats
+	'''
+	#TODO
+	pass
 
-  @click.command()
-  @click.option('--debug', is_flag=True)
-  @click.option('--threaded', is_flag=True)
-  @click.argument('HOST', default='0.0.0.0')
-  @click.argument('PORT', default=8111, type=int)
-  def run(debug, threaded, host, port):
-    """
-    This function handles command line parameters.
-    Run the server using:
+@app.route('/teams/claim/', methods=['POST'])
+def claim():
+	'''
+	POST: Process team claim of a player and redirect to team page
+	'''
+	#TODO
+	#check if already on team then update user's team after player was claimed
+	pass
 
-        python server.py
+@app.route('/teams/waive/', methods=['POST'])
+def waive():
+	'''
+	POST: Process team waive of a player and redirect to team page
+	'''
+	#TODO
+	#check if already not on team then update user's team after player was waived
+	pass
 
-    Show the help text using:
+@app.route('/players/batters/', methods=['GET'])
+def batters():
+	'''
+	GET: Render the batters page with all batters and stats
+	'''
+	#TODO
+	#check args for search and get list of all batters
+	#render batters template with data
+	pass
 
-        python server.py --help
+@app.route('/players/pitchers/', methods=['GET'])
+def pitchers():
+	'''
+	GET: Render the pitchers page with all batters and stats
+	'''
+	#TODO
+	#check args for search and get list of all pitchers
+	#render pitchers template with data
+	pass
 
-    """
+@app.route('/leagues/', methods=['GET'])
+def leagues():
+	'''
+	GET: If arguments do not have league_name key, render the leagues page with list of user's leagues
+	GET: If arguments have league_name key, render the individual league page with specific league standings
+	'''
+	#TODO
+	pass
 
-    HOST, PORT = host, port
-    print "running on %s:%d" % (HOST, PORT)
-    app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+@app.route('/leagues/transactions/', methods=['GET'])
+def leagues_transactions():
+	'''
+	GET: Using arguments with league_name key, render the league transactions page with league transactions
+	'''
+	#TODO
+	pass
 
+@app.route('/leagues/add/', methods=['POST'])
+def leagues_add():
+	'''
+	POST: Process league add and redirect to leagues page
+	'''
+	#TODO
+	pass
 
-  run()
+@app.route('/leagues/create/', methods=['GET', 'POST'])
+def leagues_create():
+	'''
+	GET: Render the create league page with static data only
+	POST: Process league create and redirect to individual league page
+	'''
+	#TODO
+	pass
+
+@app.route('/leagues/modify/', methods=['POST'])
+def leagues_modify():
+	#TODO
+
+if __name__ == '__main__':
+	import click
+
+	@click.command()
+	@click.option('--debug', is_flag=True)
+	@click.option('--threaded', is_flag=True)
+	@click.argument('HOST', default='0.0.0.0')
+	@click.argument('PORT', default=8111, type=int)
+	def run(debug, threaded, host, port):
+		HOST, PORT = host, port
+		print('running on %s:%d' % (HOST, PORT))
+		app.run(host=HOST, port=PORT, debug=debug, threaded=threaded)
+
+	run()
